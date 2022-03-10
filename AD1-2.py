89eb9e2731acdec621b2518c2eec71d210ca1f0c
Ivan Silva <ivankuartz@hotmail.com>
2022-03-10 20:48:32 -0300

    Finished function to create lists

--- AD1-2.py
+++ AD1-2.py
@@ -1,15 +1,25 @@
 from itertools import permutations
 
+def cria_listas(limite):
+    if limite == 0:
+        return []
+    elif limite == 1:
+        return [1]
+    else:
+        base = list(range(1,limite+1))*2
+        tuples = list(permutations(base))
+        listas = list(map(list, tuples))
+        return listas
+
+def verifica_condicoes(listas):
+    for i in listas:
+        for j in len(i):
 
 entrada = int(input("Por favor digite o inteiro a ser verificado"))
 
-def criaListas(limite):
-    tuples = list(permutations(range(1, limite+1)))
-    lists = list(map(list, tuples))
-    print(lists)
-    return lists
-
-criaListas(entrada)
+todas_listas = cria_listas(entrada)
 
 
-# for x in sequencia
+
+
+# # for x in sequencia
