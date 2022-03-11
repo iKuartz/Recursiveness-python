4bc05634cf27e1421e423c7972df1870e5ddb25d
Ivan Silva <ivankuartz@hotmail.com>
2022-03-10 21:10:36 -0300

    Wrote function until the part where it appends all checked list items and if the list is complete, appends the list to the results. Did not test yet.

--- AD1-2.py
+++ AD1-2.py
@@ -12,8 +12,19 @@ def cria_listas(limite):
         return listas
 
 def verifica_condicoes(listas):
+    resultados = []
+    temporarios = []
     for i in listas:
-        for j in len(i):
+        for j in range(len(i)):
+            if i[j] == i[j+i[j]+1] or i[j] == i[j-i[j]-1]:
+                temporarios.append(i[j])
+                if j == len(i) and len(temporarios) == len(i):
+                    resultados.append(temporarios)
+                    temporarios = []
+                else:
+                    temporarios = []
+            else:
+                continue
 
 entrada = int(input("Por favor digite o inteiro a ser verificado"))
 
